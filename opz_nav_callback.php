3f0928e43961e7b65a49b4673a86f0826d1eca5c
jaeheng <jaeheng@126.com>
2023-08-22 13:20:41 +0800

    callback

--- opz_nav_callback.php
+++ opz_nav_callback.php
@@ -5,12 +5,27 @@
 function callback_init()
 {
     // 生成模版配套的数据表
+    $prefix = DB_PREFIX;
+    $db = Database::getInstance();
+    $sql = "CREATE TABLE IF NOT EXISTS `{$prefix}opz_nav` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `gid` int NOT NULL COMMENT '文章ID',
+  `value` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '数据值',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `gid` (`gid`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;";
+    $db->query($sql);
 }
 
 // 插件删除时调用，可用于数据清理
 function callback_rm()
 {
     // 删除模版配套的数据表
+    // 生成模版配套的数据表
+    $prefix = DB_PREFIX;
+    $db = Database::getInstance();
+    $sql = "DROP TABLE IF EXISTS `{$prefix}opz_nav`";
+    $db->query($sql);
 }
 
 // 插件更新时调用，可用于数据库变更等
